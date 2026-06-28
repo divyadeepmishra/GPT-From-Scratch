9e2be175e85bdee7406013bed789b7e26f439227
divyadeepmishra <divydeepmishra@gmail.com>
2026-06-28 22:07:53 +0530

    Add dataset loader for next-token prediction

---
+++ dataset.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+
+class GPTDataset(Dataset):
+
+    def __init__(self, file_path, block_size):
+        self.block_size = block_size
+        self.tokens = np.fromfile(Path(file_path), dtype=np.uint16)
+
+    def __len__(self):
+        return len(self.tokens) - self.block_size
+
+    def __getitem__(self, idx):
+        x = self.tokens[idx : idx + self.block_size]
+        y = self.tokens[idx + 1 : idx + self.block_size + 1]
+        return (torch.tensor(x, dtype=torch.long), torch.tensor(y, dtype=torch.long))
+
