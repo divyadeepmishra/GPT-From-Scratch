0b1d2234aaadc900d22ecdc1fb67f7a824e4f278
divyadeepmishra <divydeepmishra@gmail.com>
2026-06-28 13:01:46 +0530

    Added feed forward layer

---
+++ feed_forward.py
@@ -0,0 +1,13 @@
+import torch.nn as nn
+
+class FeedForward(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.network = nn.Sequential(
+            nn.Linear(config.embed_dim, config.embed_dim * config.mlp_ratio),
+            nn.GELU(),
+            nn.Linear(config.embed_dim * config.mlp_ratio, config.embed_dim),
+            nn.Dropout(config.dropout)
+        )
+        def forward(self, x):
+            return self.network(x)
